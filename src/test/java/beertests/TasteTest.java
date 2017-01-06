2b5324e61b6da86e6603db52b18bf10da7ee5ef9
David Baak <davidbaak@gmail.com>
2017-01-06 15:20:58 +0100

    removed failing test

---
+++ src/test/java/beertests/TasteTest.java
@@ -0,0 +1,20 @@
+package beertests;
+
+import static app.Taste.LEMONNY;
+import static app.Taste.SPICY;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import app.IndianPaleAle;
+
+public class TasteTest {
+
+  @Test
+  public void ipa_should_taste_lemmony_and_spicy() {
+    System.out.println(new IndianPaleAle().getTastes().size());
+    MatcherAssert.assertThat(new IndianPaleAle().getTastes(), Matchers.contains(SPICY, LEMONNY));
+  }
+
+}
